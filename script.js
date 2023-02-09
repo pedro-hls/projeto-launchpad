5ff5c9b9fc7c74b374cf8714a6539a2ecad74361
Pedro Lima <18pdro@gmail.com>
2023-02-09 13:57:59 -0300

    testar o touch start

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.body.addEventListener('keyup', playSound)
-document.body.addEventListener('touchend', playSound)
+document.body.addEventListener('touchstar', playSound)
 document.body.addEventListener('keyup', press)
-document.body.addEventListener('touchend', press)
+document.body.addEventListener('touchstar', press)
 
 document.querySelector('#question-mark').addEventListener('click', sumir)
 document.querySelector('#question-mark').addEventListener('touchend', sumir)
